6ea3e26c8f15e0cd0b79a6d8c22cb1604f397025
Alica Domanyova <11502862@cuni.cz>
2025-10-28 12:51:36 +0100

    first run (full of bugs)

---
+++ glucose-4.2.1/sources/simp/depend.mk
@@ -0,0 +1,42 @@
+/home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/Main.o /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/Main.or /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/Main.od /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/Main.op: \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/Main.cc \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../utils/System.h \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../mtl/IntTypes.h \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../utils/ParseUtils.h \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../utils/Options.h \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../mtl/Vec.h \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../mtl/XAlloc.h \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../core/Dimacs.h \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../core/SolverTypes.h \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../mtl/Alg.h \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../mtl/Map.h \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../mtl/Alloc.h \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../simp/SimpSolver.h \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../mtl/Queue.h \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../core/Solver.h \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../mtl/Heap.h \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../core/BoundedQueue.h \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../core/Constants.h \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../mtl/Clone.h \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../core/SolverStats.h
+/home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/SimpSolver.o /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/SimpSolver.or /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/SimpSolver.od /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/SimpSolver.op: \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/SimpSolver.cc \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../mtl/Sort.h \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../mtl/Vec.h \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../mtl/IntTypes.h \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../mtl/XAlloc.h \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../simp/SimpSolver.h \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../mtl/Queue.h \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../core/Solver.h \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../mtl/Heap.h \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../mtl/Alg.h \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../utils/Options.h \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../utils/ParseUtils.h \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../core/SolverTypes.h \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../mtl/Map.h \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../mtl/Alloc.h \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../core/BoundedQueue.h \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../core/Constants.h \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../mtl/Clone.h \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../core/SolverStats.h \
+ /home/alica/school/logika/project/SAT-project/glucose-4.2.1/sources/simp/../utils/System.h
--- maximumClique.py
+++ maximumClique.py
@@ -1,10 +1,6 @@
 import subprocess
 from argparse import ArgumentParser
 
-V = 0
-E = 0
-adjacencyMatrix = []
-
 
 def vertexAtomicNumber(v):
     # atomic formulas representing "vertex v is a part of chosen clique"
@@ -27,21 +23,26 @@ def inputParser(input_file_name):
                 V = int(lineList[2])
                 E = int(lineList[3])
                 adjacencyMatrix = [[0] * V] * V
+                #print(V, E)
                 
             elif lineList[0] == "e":
                 v1, v2 = int(lineList[1]), int(lineList[2])
+                v1 -= 1
+                v2 -= 1
                 adjacencyMatrix[v1][v2] = 1
                 adjacencyMatrix[v2][v1] = 1
             else: 
-                print("WRONG INPUT")
+                #print("WRONG INPUT")
                 exit(0)
+    return V, E, adjacencyMatrix
 
 def call_solver(cnf, nr_vars, output_name, solver_name, verbosity):
+    #print(cnf)
     # print CNF into formula.cnf in DIMACS format
     with open(output_name, "w") as file:
         file.write("p cnf " + str(nr_vars) + " " + str(len(cnf)) + '\n')
         for clause in cnf:
-            file.write(' '.join(str(lit) for lit in clause) + '\n')
+            file.write(' '.join(str(lit) for lit in clause) + '\n') # maybe 0 at the end??
 
     # call the solver and return the output
     return subprocess.run(['./' + solver_name, '-model', '-verb=' + str(verbosity) , output_name], stdout=subprocess.PIPE)
@@ -52,8 +53,8 @@ def graphLogic():
     # add formulas representing edges
     for i in range(V):
         for j in range(V):
-            if adjacencyMatrix[i][j]: temp_cnf.append(edgeAtomicNumber(i,j))
-            else: temp_cnf.append( (- edgeAtomicNumber(i,j)))
+            if adjacencyMatrix[i][j]: temp_cnf.append([edgeAtomicNumber(i,j)])
+            else: temp_cnf.append([- edgeAtomicNumber(i,j)])
 
     # add formulas representing vertices in cliques
     # v_1 && v_2 -> e_1,2
@@ -61,7 +62,7 @@ def graphLogic():
     for v1 in range(V):
         for v2 in range(V):
             if v1 == v2: continue
-            temp_cnf.append( (-vertexAtomicNumber(v1), -vertexAtomicNumber(v2), edgeAtomicNumber(v1,v2)))
+            temp_cnf.append( [-vertexAtomicNumber(v1), -vertexAtomicNumber(v2), edgeAtomicNumber(v1,v2)])
  
     return temp_cnf
 
@@ -76,19 +77,20 @@ def counterLogic():
     # not s_i,j or s_i+1,j
     for i in range(V - 1):
         for j in range(V):
-            temp_cnf.append((-seqCounterNumber(i,j), seqCounterNumber(i+1,j)))
+            temp_cnf.append([-seqCounterNumber(i,j), seqCounterNumber(i+1,j)])
 
     # s_1,1 <-> v
     # (s_1,1 or not v) and (x or not s_1,1)
-    temp_cnf.append((seqCounterNumber(0,0), -vertexAtomicNumber(0)))
-    temp_cnf.append((-seqCounterNumber(0,0), vertexAtomicNumber(0)))
+    temp_cnf.append([seqCounterNumber(0,0), -vertexAtomicNumber(0)])
+    temp_cnf.append([-seqCounterNumber(0,0), vertexAtomicNumber(0)])
     # if vertex v is in clique, then increment
     # (v and s_v-1,j) -> s_v,j+1
     # not v or not s_v-1,j or s_v,j+1
     for v in range(1, V):
         for j in range(V-1):
-            temp_cnf.append(-vertexAtomicNumber(v), -seqCounterNumber(v-1, j), seqCounterNumber(v, j+1))
+            temp_cnf.append([-vertexAtomicNumber(v), -seqCounterNumber(v-1, j), seqCounterNumber(v, j+1)])
 
+    return temp_cnf
 
 def getCliqueVertices(result):
     # parse the model from the output of the solver
@@ -132,7 +134,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "-s",
         "--solver",
-        default="glucose-syrup",
+        default="glucose-4.2.1/sources/simp/glucose",
         type=str,
         help=(
             "The SAT solver to be used."
@@ -151,14 +153,16 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # get the input instance
-    inputParser(args.input)
+    V, E, adjacencyMatrix = inputParser(args.input)
 
-    # call the SAT solver and get the result
-    nr_vars = V + 2*V*V
+    nr_vars = V + 2*V*V + 1
+    #print(nr_vars)
 
     cnf = []
     cnf.extend(graphLogic())
     cnf.extend(counterLogic())
+    print(cnf)
+    exit(0)
 
     # now we just binary search the biggest possible amount of verteces in cliques
     smallEnd = 0
@@ -166,7 +170,7 @@ if __name__ == "__main__":
     while b*2 < V: b *= 2
     while b > 0:
         k = smallEnd + b
-        cnf.append(seqCounterNumber(V-1, k))
+        cnf.append([seqCounterNumber(V-1, k)])
         # TODO: solve cnf
         # if solution: smallEnd = k
         result = call_solver(cnf, nr_vars, args.output, args.solver, args.verb)
@@ -185,19 +189,3 @@ if __name__ == "__main__":
     print(" ".join(clique))
     
     
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
